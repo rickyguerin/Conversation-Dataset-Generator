de96c95592fb04ebbb8f93be4cf7daf7cd4b4a4f
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-02-07 22:21:17 -0500

    Add script

---
+++ Assets/Scripts/HeadBob.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HeadBob : MonoBehaviour
+{
+        public int seed = 0;
+
+        // Update is called once per frame
+        void Update()
+        {
+                transform.Rotate(Mathf.Cos(Mathf.PI * Time.time) * 20, 0, 0);
+        }
+}
