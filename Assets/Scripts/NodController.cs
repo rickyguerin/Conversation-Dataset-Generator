dad22eae45ba41a20e438d382bc2676c0f2b6fb2
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-02-08 14:55:30 -0500

    Add Nodders to controller

--- Assets/Scripts/NodController.cs
+++ Assets/Scripts/NodController.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NodController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public Nodder[] nodders;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+                foreach(Nodder n in nodders)
+                {
+                        n.SetAngle(Random.Range(5, 10));
+                        n.SetSpeed(Random.Range(0.5f, 2.0f));
+                }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                        nodders[0].AddNods(Random.Range(1, 5));
+                }
+
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                        nodders[1].AddNods(Random.Range(1, 5));
+                }
+        }
 }
