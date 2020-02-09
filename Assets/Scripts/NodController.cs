6a23af204295c01a62297215c516eaec6961c81f
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-02-09 01:12:13 -0500

    Implemented automated video recording

--- Assets/Scripts/NodController.cs
+++ Assets/Scripts/NodController.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class NodController : MonoBehaviour
 {
+        // All of the heads to nod
         public Nodder[] nodders;
 
-        // Start is called before the first frame update
+        // Number of videos to record
+        public int numVideos = 0;
+
+        // Is capt currently recording?
+        private bool recording;
+
         void Start()
         {
+                recording = false;
+
                 foreach(Nodder n in nodders)
                 {
                         n.SetAngle(Random.Range(5, 10));
@@ -14,9 +23,18 @@ public class NodController : MonoBehaviour
                 }
         }
 
-        // Update is called once per frame
         void Update()
         {
+                if (numVideos == 0)
+                {
+                        Application.Quit();
+                }
+
+                else if (!recording)
+                {
+                        StartCoroutine("RecordVideo");
+                }
+
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                         nodders[0].AddNods(Random.Range(1, 5));
@@ -26,5 +44,19 @@ public class NodController : MonoBehaviour
                 {
                         nodders[1].AddNods(Random.Range(1, 5));
                 }
+        }
+
+        IEnumerator RecordVideo()
+        {
+                recording = true;
+
+                RockVR.Video.VideoCaptureCtrl.instance.StartCapture();
+
+                yield return new WaitForSeconds(60);
+
+                RockVR.Video.VideoCaptureCtrl.instance.StopCapture();
+
+                numVideos--;
+                recording = false;
         }
 }
