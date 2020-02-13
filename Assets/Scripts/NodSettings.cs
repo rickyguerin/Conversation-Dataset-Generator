517d85aa68c044eae81ba1880a1933ade6c7020c
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-02-13 14:01:19 -0500

    Add other nod functions

--- Assets/Scripts/NodSettings.cs
+++ Assets/Scripts/NodSettings.cs
@@ -22,4 +22,40 @@ public abstract class NodSettings
 
 		return 0;
 	}
+
+	// Return a nod angle within an appropriate range for the given engagement level
+	public static float NodAngle(EngagementLevel level)
+	{
+		switch (level)
+		{
+			case EngagementLevel.LOW:
+				return Random.Range(5.0f, 8.0f);
+			case EngagementLevel.MEDIUM:
+				return Random.Range(8.0f, 10.0f);
+			case EngagementLevel.HIGH:
+				return Random.Range(10.0f, 12.0f);
+			default:
+				break;
+		}
+
+		return 0.0f;
+	}
+
+	// Return a nod speed within an appropriate range for the given engagement level
+	public static float NodSpeed(EngagementLevel level)
+	{
+		switch (level)
+		{
+			case EngagementLevel.LOW:
+				return Random.Range(5.0f, 8.0f);
+			case EngagementLevel.MEDIUM:
+				return Random.Range(8.0f, 10.0f);
+			case EngagementLevel.HIGH:
+				return Random.Range(10.0f, 12.0f);
+			default:
+				break;
+		}
+
+		return 0.0f;
+	}
 }
