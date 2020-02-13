6106a4f1f5193870da766f983cf070af00979c2c
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-02-13 14:01:04 -0500

    Use Unity Random

--- Assets/Scripts/NodSettings.cs
+++ Assets/Scripts/NodSettings.cs
@@ -1,24 +1,21 @@
-﻿using System;
+﻿using UnityEngine;
 
 public abstract class NodSettings
 {
-	// Random number generator
-	public static Random rand = new Random();
-
 	// Possible values for level of engagement
 	public enum EngagementLevel { LOW, MEDIUM, HIGH }
 
 	// Return a number of nods within an appropriate range for the given engagement level
-	public static int Nods(EngagementLevel level)
+	public static int NodsToDo(EngagementLevel level)
 	{
 		switch (level)
 		{
 			case EngagementLevel.LOW:
-				return rand.Next(0, 3);
+				return Random.Range(0, 3);
 			case EngagementLevel.MEDIUM:
-				return rand.Next(3, 6);
+				return Random.Range(3, 6);
 			case EngagementLevel.HIGH:
-				return rand.Next(6, 10);
+				return Random.Range(6, 10);
 			default:
 				break;
 		}
