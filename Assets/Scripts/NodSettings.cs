0922949afd5d7e42f883a7c8e7c5f58245dec16b
Ricky Guerin <Guerin.Ricky@gmail.com>
2020-02-22 16:03:15 -0500

    Add InteractionRate

--- Assets/Scripts/NodSettings.cs
+++ Assets/Scripts/NodSettings.cs
@@ -5,6 +5,25 @@ public abstract class NodSettings
 	// Possible values for level of engagement
 	public enum EngagementLevel { LOW, MEDIUM, HIGH }
 
+	// Return a float 
+	public static float InteractionRate(EngagementLevel level)
+	{
+		switch (level)
+		{
+			case EngagementLevel.LOW:
+				return Random.Range(0.0f, 0.2f);
+			case EngagementLevel.MEDIUM:
+				return Random.Range(0.4f, 0.6f);
+			case EngagementLevel.HIGH:
+				return Random.Range(0.8f, 1.0f);
+			default:
+				break;
+		}
+
+		return 0;
+	}
+
+
 	// Return a number of nods within an appropriate range for the given engagement level
 	public static int NodsToDo(EngagementLevel level)
 	{
